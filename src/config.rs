52eb9735493a0b95a6dee2c545f9abe864e78615
鬼鬼Sama <54203072+Akuma-real@users.noreply.github.com>
2025-06-01 00:10:55 +0800

    fix: 更新RustDesk服务器地址和公钥
    将默认的RustDesk服务器地址从"rs-ny.rustdesk.com"更改为"47.122.123.99"，并更新对应的RS公钥。这些变更是为了切换到新的服务器配置。

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["47.122.123.99"];
+pub const RS_PUB_KEY: &str = "rP2tWOXR8tZNIt9fTPfDxM5t+5zI512KWcdHi+Xpd28=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
